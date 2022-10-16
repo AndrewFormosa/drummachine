e1c995e18526397680a8a6a22f019d5a6d63c11b
Andrew Formosa <andyformosa@aol.com>
2022-10-16 19:03:50 +0100

    1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
 import logo from './logo.svg';
 import './App.scss';
+import React from 'react';
+
+
+class DrumMachine extends React.Component{
+constructor(props){
+  super(props);
+  this.state={};
+}
+
+
+render(){
+  return(
+    <div className='container-fluid'>
+      <p>my drun machine</p>
+      <div class="row justify-content-md-center align-middle">
+  <div class="col-md-auto">
+    1 of 3
+  </div>
+  <div class="col-md-auto box">
+    Variable width content asdasd
+  </div>
+</div>
+    </div>
+  );
+}
+
+}
+
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <button className='btn btn-primary'>test</button>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+         <DrumMachine/> 
+      </div>
+
   );
 }
 
