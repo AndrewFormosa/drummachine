3f4816ce85495b28798558d066d9fb04625c1b8b
Andrew Formosa <andyformosa@aol.com>
2022-10-17 20:55:30 +0100

    1

--- src/App.js
+++ src/App.js
@@ -3,23 +3,74 @@ import './App.scss';
 import React from 'react';
 
 
+class DrumPadPanel extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  render() {
+    return (
+
+      <div className='container-flex'>
+        <div className="row justifty-content-around g-2 my-1 ">
+          <div className="col-4">
+            <button class="drum-pad btn btn-secondary">Q</button>
+          </div>
+          <div className="col-4">
+            <button className="drum-pad btn btn-secondary">W</button>
+          </div>
+          <div className="col-4">
+            <button className="drum-pad btn btn-secondary">E</button>
+          </div>
+        </div>
+        <div className="row justifty-content-around g-2 ">
+          <div className="col-4">
+            <button className="drum-pad btn btn-secondary">A</button>
+          </div>
+          <div className="col-4">
+            <button class="drum-pad btn btn-secondary">S</button>
+          </div>
+          <div className="col-4">
+            <button className="drum-pad btn btn-secondary">D</button>
+          </div>
+        </div>
+         <div className="row justifty-content-around g-2 ">
+          <div className="col-4">
+            <button className="drum-pad btn btn-secondary">Z</button>
+          </div>
+          <div className="col-4">
+            <button class="drum-pad btn btn-secondary">X</button>
+          </div>
+          <div className="col-4">
+            <button className="drum-pad btn btn-secondary">C</button>
+          </div>
+        </div>
+      </div>
+
+
+    );
+  }
+}
+
+
+
+
+
 class DrumMachine extends React.Component {
   constructor(props) {
     super(props);
     this.state = {};
   }
 
-
   render() {
     return (
       <div className='container'>
         <p>my drun machine</p>
         <div className="row justify-content-center">
-          <div className="col-sm-12  box-one">Controlls</div>
-          <div className="col-sm-12  box-two">DrumPad</div>         
-          </div>
+          <div className="col-sm-12  box-one"><DrumPadPanel /></div>
+          <div className="col-sm-12  box-two">DrumPad</div>
         </div>
-    
+      </div>
     );
   }
 
