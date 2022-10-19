b3aa63005372cb95c698b4ebf684492fd0bef7a4
Andrew Formosa <andyformosa@aol.com>
2022-10-19 19:57:09 +0100

    1

--- src/App.js
+++ src/App.js
@@ -1,6 +1,13 @@
 import logo from './logo.svg';
 import './App.scss';
 import React from 'react';
+//import mySound from "./drumSounds/Kick_3.wav";
+import s00 from "./drumSounds/Kick_3.wav";
+import s01 from "./drumSounds/Snare_3.WAV";
+import s10 from "./drumSounds/Kick_3.wav";
+import s11 from "./drumSounds/Snare_3.WAV";
+
+const a=[{"A":s00, "B":s01},{"A":s10,"B":s11}];
 
 
 class DrumPadPanel extends React.Component {
@@ -12,7 +19,7 @@ class DrumPadPanel extends React.Component {
     return (
 
       <div className='container-flex'>
-        <div className="row justifty-content-around g-2 my-1 ">
+        <div className="row g-2 my-1 ">
           <div className="col-4">
             <button class="drum-pad btn btn-secondary">Q</button>
           </div>
@@ -23,7 +30,7 @@ class DrumPadPanel extends React.Component {
             <button className="drum-pad btn btn-secondary">E</button>
           </div>
         </div>
-        <div className="row justifty-content-around g-2 ">
+        <div className="row g-2 ">
           <div className="col-4">
             <button className="drum-pad btn btn-secondary">A</button>
           </div>
@@ -34,7 +41,7 @@ class DrumPadPanel extends React.Component {
             <button className="drum-pad btn btn-secondary">D</button>
           </div>
         </div>
-         <div className="row justifty-content-around g-2 ">
+         <div className="row g-2 ">
           <div className="col-4">
             <button className="drum-pad btn btn-secondary">Z</button>
           </div>
@@ -59,15 +66,40 @@ class DrumPadPanel extends React.Component {
 class DrumMachine extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+
+      audio:new Audio(a[0].B),
+      isPlaying:false,
+      
+
+    };
+
+    this.playSound=this.playSound.bind(this);
+
   }
 
+
+
+
+  playSound=(val)=>{
+   //this.setState({audio: new Audio(s00)});
+    let myAudio=new Audio(a[0].A);
+    myAudio.play();
+  }
+
+
   render() {
+
+
+
     return (
+    
+
       <div className='container'>
         <p>my drun machine</p>
         <div className="row justify-content-center">
           <div className="col-sm-12  box-one"><DrumPadPanel /></div>
+          <button class="btn btn-primary" onClick={this.playSound}>ckilckMe</button>
           <div className="col-sm-12  box-two">DrumPad</div>
         </div>
       </div>
@@ -79,6 +111,10 @@ class DrumMachine extends React.Component {
 
 function App() {
   return (
+
+
+
+
     <div className="d-flex App min-vh-100 border border-primary">
       <div className="d-flex m-auto border border-primary align-items-center">
         <DrumMachine />
